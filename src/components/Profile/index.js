24a9a5435b2f2d8a8625aa38688170a624acb5fb
Daniel Oliveira <danielp.oliveira@outlook.com>
2020-04-05 16:32:33 -0300

    adding Prisma Playground

---
+++ src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Daniel Oliveira</h1>
+    <h2>Desenvolvedor Backend</h2>
+    <p>
+      Membro do Core Team da Empresa Foo, escrevo sobre o mundo backend,
+      viagens, vida pessoal e mais.
+    </p>
+  </div>
+)
+
+export default Profile
