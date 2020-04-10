80f8c949ba5280366ae7cf1fb5d6ce11f568c043
Daniel Oliveira <danielp.oliveira@outlook.com>
2020-04-10 16:30:06 -0300

    Configure manifest, sitemap, seo, pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,7 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-sitemap`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -76,14 +77,19 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
+        name: `John Doe`,
+        short_name: `John Doe`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
